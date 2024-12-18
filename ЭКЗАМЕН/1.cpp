b50df8fbfbb0cbf9299bac29cfca0b2490a83db4
Ivan24-wq <iv8747583@gmail.com>
2024-12-18 13:32:33 +0300

    Использовал метод класса

--- ЭКЗАМЕН/1.cpp
+++ ЭКЗАМЕН/1.cpp
@@ -2,35 +2,28 @@
 #include <vector>
 using namespace std;
 
-#include <iostream>
-#include <vector>
-using namespace std;
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n){
+        int i = m - 1;  // Указываем на последний элемент первого массива
+        int j = n - 1;  // Указываем на последний элемент второго массива
+        int k = m + n - 1;  // Указываем на последний индекс в объединенном массиве
 
-// Класс Solution с методом merge
-#include <iostream>
-#include <vector>
-using namespace std;
+        // Объединение массивов
+        while(i >= 0 && j >= 0){
+            if(nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];  // Копируем из nums1
+            } else {
+                nums1[k--] = nums2[j--];  // Копируем из nums2
+            }
+        }
 
-void merge(vector<int>& nums1, int m, vector<int>& nums2, int n){
-    int i = m - 1;
-    int j = n - 1;
-    int k = m + n;
-
-    // Объединение массивов
-
-    while(i >= 0 && j >= 0){
-        if(nums1[i] > nums2[j]) {
-            nums1[k--] = nums2[i--];
-        } else {
-            nums1[k--] = nums2[j--];
+        // Если остались элементы в nums2
+        while(j >= 0){
+            nums1[k--] = nums2[j--];  // Копируем оставшиеся элементы из nums2
         }
     }
-    while(j >= 0){
-        nums1[k--] = nums2[j--];
-    }
-}
-
-
+};
 
 int main(){
     setlocale(LC_ALL, "RU");
@@ -39,36 +32,35 @@ int main(){
     cin >> n;
     cout << "Введите количество элементов для второго массива: ";
     cin >> m;
-    vector<int> nums1(n+m, 0);
+
+    // Инициализируем массивы
+    vector<int> nums1(n + m, 0);  // Первый массив с дополнительным пространством
     vector<int> nums2(m);
 
-    cout << "Введите" << n << "Элементы nums1: ";
+    // Ввод первого массива
+    cout << "Введите " << n << " элементов nums1: ";
     for(int i = 0; i < n; ++i){
         cin >> nums1[i];
     }
 
-    cout << "Введите" << m << "Элементы nums1: ";
+    // Ввод второго массива
+    cout << "Введите " << m << " элементов nums2: ";
     for(int j = 0; j < m; ++j){
-        cin >> nums2[m];
+        cin >> nums2[j];
     }
 
-    //Объединим массмивы
-    merge(nums1, n, nums2, m);
+    // Создаем объект класса Solution
+    Solution solution;
 
+    // Объединяем массивы
+    solution.merge(nums1, n, nums2, m);
+
+    // Выводим результат
     cout << "Ответ: ";
     for(int num : nums1){
         cout << num << " ";
     }
     cout << endl;
+
     return 0;
 }
-
-
-//  Задача
-// Вам даны два целочисленных массива nums1и nums2, отсортированных в неубывающем порядке , и два целых числа mи n, представляющие количество элементов в nums1и nums2соответственно.
-
-//Объединить nums1 в nums2один массив, отсортированный в неубывающем порядке .
-
-//Окончательный отсортированный массив не должен возвращаться функцией, а должен храниться внутри массиваnums1 . Чтобы учесть это, nums1имеет длину m + n, где первые mэлементы обозначают элементы, которые должны быть объединены, а последние nэлементы установлены в 0и должны игнорироваться. nums2имеет длину n.
-
- 
